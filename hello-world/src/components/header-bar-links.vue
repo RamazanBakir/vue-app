80cbbed09fd4cc2660f12e392d9e39fc28113894
Ramazan Bakır <48602486+RamazanBakir@users.noreply.github.com>
2021-02-07 23:13:22 +0300

    Heroes Form Application

---
+++ hello-world/src/components/header-bar-links.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a
+              :href="github"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <!-- href tagında (:) kullandık bu bir kısaltmadır ve içinde olan variable'da
+          export tarafında belirtilmiştir. -->
+          <a
+              v-bind:href="twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name : 'HeaderBarLinks',
+  data() {
+    return {
+      github: 'https://github.com/RamazanBakir/',
+      twitter: 'https://twitter.com/',
+    };
+  },
+}
+</script>
